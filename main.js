45d7d03298dc79581fb1aa0c2c72ac3ed64fb95c
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-04-07 14:21:56 +0100

    update

---
+++ main.js
@@ -0,0 +1,41 @@
+
+//objects in an array
+let carOne = {
+    make: "Ferrari",
+    model: "Testerossa",
+    style: "Performance",
+    topSpeed:135,
+    acceleration: 5.7,
+};
+
+let carTwo = {
+    make: "Vauxhaill",
+    model: "Corsa",
+    style: "Hatchback",
+    topSpeed: 124,
+    acceleration: 6.9,
+};
+
+let carThree = {
+    make: "Mini",
+    model: "Cooper",
+    style: "Hatchback",
+    topSpeed: 109,
+    acceleration: 12.2,
+};
+
+const cars = [carOne, carTwo, carThree];
+
+//numbers and strings in an array
+
+//Multi Dimensional Arrays
+//Stacks: LIFO - push/pop
+//Queues FIFO - push/pop
+//Linked Lists: Address for next item stored allows to dynamically add/remove from list
+//example of linked list from 
+
+//LOOPS
+//Use For loops when the number of times to execute loop is known. 
+//Use while loop when this is unknown
+//use do/while when want to execute code at least once
+//use BREAK command to get out of a loop
